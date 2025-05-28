b630295e0a1b9070fdd50cd1ff3fbfea86923902
VoiceZero <dc7_83@icloud.com>
2025-05-28 17:19:38 +0200

    Update lang.js

--- lang.js
+++ lang.js
@@ -12,7 +12,7 @@ async function translate(lang) {
 
     document.querySelector('#form h2').textContent = data.formTitle;
     document.querySelector('label[for="name"]').textContent = data.nameLabel;
-    document.querySelector('#name').placeholder = data.namePlaceholder;
+    document.querySelector('input[name="name"]').placeholder = data.namePlaceholder;
     document.querySelector('label[for="format"]').textContent = data.formatLabel;
     document.querySelector('label[for="text"]').textContent = data.textLabel;
     document.querySelector('label[for="video"]').textContent = data.videoLabel;
@@ -24,7 +24,7 @@ async function translate(lang) {
     document.getElementById('anonymous-note').textContent = data.anonymousNote;
 
     document.querySelector('button[type="submit"]').textContent = data.submitButton;
-    document.querySelector('.dev-banner').textContent = "🛠 " + data.headerSubtitle;
+    document.querySelector('.dev-banner').textContent = data.devBanner;
   } catch (error) {
     console.error("Errore nel caricamento della lingua:", error);
   }
